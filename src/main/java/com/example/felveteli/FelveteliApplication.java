53426ad777b35cd7c8663ce74b844a51410d98ff
DAndris <pajszerm5@gmail.com>
2024-01-02 17:10:56 +0100

    initial commit

---
+++ src/main/java/com/example/felveteli/FelveteliApplication.java
@@ -0,0 +1,13 @@
+package com.example.felveteli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelveteliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelveteliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/felveteli/FelveteliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.felveteli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelveteliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
